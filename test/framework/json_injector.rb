1e831a9c800a2a065e4bc20ff7f73b7d09d75906
MattWVile <matt.vile@yahoo.co.uk>
2022-04-12 13:49:25 +0100

    made all id be extracted from the json

--- test/framework/json_injector.rb
+++ test/framework/json_injector.rb
@@ -4,8 +4,10 @@ require_relative 'api_reader.rb'
 class JsonInjector
 
   def getHashMapResponse
+    idCount = 0
     hashMap = Hash.new
     json = ApiReader.new.getJsonResponse
+    puts json
     parsedJson = JSON.parse(json)
     parsedJson.each do |key, value|
       if value.class == Array
@@ -13,7 +15,14 @@ class JsonInjector
       end
       if value.class == Hash
         value.each do |key1, value1|
-          hashMap[key1] = value1
+          if key1 == "id" && idCount == 0
+            hashMap["weather_id"] = value1
+            idCount += 1
+          elsif key1 == "id" && idCount == 1
+            hashMap["system_id"] = value1
+          else
+            hashMap[key1] = value1
+          end
         end
       else
         hashMap[key] = value
